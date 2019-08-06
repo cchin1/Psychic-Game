7806214b0a3e09f6f871abdc8a1cac687a33e2eb
Candice Chin <chincandice2007@gmail.com>
2019-08-06 11:46:07 -0700

    edited style.css

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -9,3 +9,101 @@
 8. When the player loses, increase the Losses counter and restart the game without a page refresh (just like when the user wins).
 */
 
+var lettersGuessed = [];
+var guessesLeft = 10;
+var wins = 0;
+
+
+
+// using Math.random method along with String.fromCharCode method to generate random selection of random letter
+var computerGuess = 
+    String.fromCharCode(
+        Math.round(Math.random() * 26) + 97
+    );
+
+console.log(computerGuess);
+
+// function to capture user's keyboard input and make the input lowercase
+document.onkeydown = function(event) {
+    var keyPress = (String.fromCharCode(event.keyCode)).toLowerCase();
+
+    //document.getElementById('guess').innerHTML = keyPress;
+    addLetter(keyPress);
+
+}
+
+//function to catch repeat letter and/or add players guess to lettersGuessed
+function addLetter (usersKeypress) {
+
+    var repeatGuess = lettersGuessed.some(function(item){
+        return item ===usersKeypress;
+    })
+
+    //alert player if the above code is true.
+    if (repeatGuess) {
+        alert(usersKeypress + " already guessed. try again");
+
+        //if it is not a repeat guess, check if it's in word
+    } else {
+        lettersGuessed.push(usersKeypress);
+        console.log(lettersGuessed);
+
+        //show user's input in browser
+        showLettersGuessed();
+
+        //is user's input a match to computer guess
+        guessMatch(usersKeypress);
+    }
+
+}
+
+//function to show letters guessed in browser
+function showLettersGuessed() {
+    var tempStr = lettersGuessed.join(", ");
+    document.getElementById("playersGuess".innerHTML = tempStr;
+}
+
+function guessMatch (character) {
+
+    console.log(character);
+    console.log(computerGuess);
+
+    if (character === computerGuess) {
+
+        alert("You win!");
+        wins = wins + 1;
+        showWins();
+        //toggleGame();
+
+    } else if (guessesLeft === 0) {
+        
+        alert("Sorry, need to start over.");
+        resetVariables ();
+
+    } else {
+        guessesLeft = guessesLeft - 1;
+        showGuessesRemaining();
+    }   
+}
+
+//function to show wins
+function showWins() {
+        document.getElementById("numWins").innerHTML = wins;
+}
+    
+//function to show guesses remaining
+function showGuessesRemaining() {
+    document.getElementById("numGuesses").innerHTML = guessesLeft;
+}
+
+function resetVariables () {
+    lettersGuessed = []
+    guessesLeft = 10;
+}
+
+function startGame() {
+    showGuessesRemaining();
+    showWins();
+}
+// to make the computer guess another random letter once game is reset
+startGame();
